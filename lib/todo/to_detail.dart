7def0b0c724baccae276f7a7cdc3dd371bcb0315
zhenghuachao <zhcandroid@163.com>
2025-07-07 16:05:17 +0800

    使用riverpod实现todolist

---
+++ lib/todo/to_detail.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+///  author : zhc 2025/7/6 22:40
+///  desc   :
+
+class TodoDetail extends StatefulWidget {
+  const TodoDetail({super.key});
+
+  @override
+  State<TodoDetail> createState() => _TodoDetailState();
+}
+
+class _TodoDetailState extends State<TodoDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
